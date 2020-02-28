620b76d5f446f29c330169c8c6c8b04f57d31b26
felix <felix375@gmail.com>
2020-02-28 07:03:38 -0500

    Added Comments

--- Random Genetics File Gen.py
+++ Random Genetics File Gen.py
@@ -2,9 +2,16 @@
 import random
 dna_letter = ['A', 'C', 'T', 'G']
 sequence = ""
+
+# seq_length multiplied by 1000 in final version
 seq_length = input("Length of Sequence (in multiples of 1000): ")
+
+# actual length determined by multiplier
 length = int(seq_length) * 100
+
 file_name = input("File Name: ")
+
+# generate actual random DNA sequence
 for x in range(length):
     sequence += random.choice(dna_letter)
 print(sequence)
