0e8e1fcc250eae10535617e72c24d48ef0bfe34b
felix <felix375@gmail.com>
2020-02-28 10:24:00 -0500

    Added execution time check code 10,000 bases takes approx 0.01 seconds

--- Random Genetics File Gen.py
+++ Random Genetics File Gen.py
@@ -1,4 +1,5 @@
-# Generates random DNA data file
+# This program generates random DNA data file of arbitrary size
+
 import random
 dna_letter = ['A', 'C', 'T', 'G']
 sequence = ""
@@ -9,9 +10,18 @@ seq_length = input("Length of Sequence (in multiples of 1000): ")
 # actual length determined by multiplier
 length = int(seq_length) * 1000
 
+# put each 1000 dna sequence into a separate string for writing to file
+
 file_name = input("File Name: ")
+
+# Code Execution time code
+# import time
+# start_time = time.time()
 
 # generate actual random DNA sequence
 for x in range(length):
     sequence += random.choice(dna_letter)
 print(sequence)
+
+# Print Code execution time
+# print ("time elapsed: {:.2f}s".format(time.time() - start_time))
